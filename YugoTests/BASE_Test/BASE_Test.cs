90d48e87966cb121c4072a4554a096420bb2b77b
Artem Sukharevskyi <sucharevskiy.artem.91@gmail.com>
2023-04-19 19:35:57 +0300

    Add tests project to solution

---
+++ YugoTests/BASE_Test/BASE_Test.cs
@@ -0,0 +1,24 @@
+﻿using Base_Temlate.Helpers;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Test.BASE_Test
+{
+    [AllureNUnit]
+    public class BASE : BaseWeb
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Browser._Driver.Manage().Window
+                           .Maximize();
+            Browser._Driver.Navigate()
+                           .GoToUrl(Endpoints.BASE_URL);
+        }
+    }
+}
---
+++ YugoTests/TEST/TemplateTest.cs
@@ -0,0 +1,29 @@
+﻿using Base_Temlate.PageObjects;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template_Test.BASE_Test;
+
+namespace Template_Test.TEST
+{
+    [AllureNUnit]
+    [TestFixture]
+    public class Yugo : BASE
+    {
+        [Test]
+        public void Login()
+        {
+            Pages.Login
+                .SignIn();
+            Pages.Vote
+                .Voting();
+            Pages.Vote
+                .Voting();
+
+        }
+    }
+}
